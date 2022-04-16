f13a91d3f0dbd37be7a6c0fdb4f86d088d2623fc
Zdzislaw <zdzislaw22@windowslive.com>
2022-04-16 07:24:36 +0200

    SL config fix

--- StreamlinkConfig/plugin.py
+++ StreamlinkConfig/plugin.py
@@ -1,11 +1,15 @@
 from __future__ import absolute_import #zmiana strategii ladowanie modulow w py2 z relative na absolute jak w py3
 from Plugins.Plugin import PluginDescriptor
-from Plugins.Extensions.StreamlinkConfig.__init__ import mygettext as _
+from . import mygettext as _
+import sys
+
+if sys.version_info.major > 2: #PyMajorVersion
+    from importlib import reload
 
 def main(session, **kwargs):
     import Plugins.Extensions.StreamlinkConfig.StreamlinkConfiguration
     reload(Plugins.Extensions.StreamlinkConfig.StreamlinkConfiguration)
-    session.open(StreamlinkConfiguration.StreamlinkConfiguration)
+    session.open(Plugins.Extensions.StreamlinkConfig.StreamlinkConfiguration.StreamlinkConfiguration)
 
 def Plugins(path, **kwargs):
     return [PluginDescriptor(name=_("Streamlink Configuration"), where = PluginDescriptor.WHERE_PLUGINMENU, fnc = main, needsRestart = False)]
