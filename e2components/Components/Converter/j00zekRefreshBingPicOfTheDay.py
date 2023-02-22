805edb52ffb54963ddffb27375188de7b692673a
Zdzislaw <zdzislaw22@windowslive.com>
2023-02-22 06:12:54 +0100

    ulepszenia binga w e2c

--- e2components/Components/Converter/j00zekRefreshBingPicOfTheDay.py
+++ e2components/Components/Converter/j00zekRefreshBingPicOfTheDay.py
@@ -17,17 +17,8 @@ class j00zekRefreshBingPicOfTheDay(Converter, object):
         Converter.__init__(self, arg)
         if DBG: j00zekDEBUG('j00zekRefreshBingPicOfTheDay(Converter).__init__ >>>')
         self.AlternatePicPath = arg
-        self.BingPic_jpg = '/usr/share/enigma2/BlackHarmony/icons/BingPicOfTheDay.jpg'
-        self.BingPic_png = '/usr/share/enigma2/BlackHarmony/icons/BingPicOfTheDay.png'
+        self.BingPic = self.AlternatePicPath.replace('.png','-Bing.png').replace('.jpg','-Bing.jpg')
         self.Init = True
-        self.isVTI = os.path.exists('/usr/lib/enigma2/python/Plugins/SystemPlugins/VTIPanel/')
-        try:
-            if open("/proc/stb/info/brandname", "r").read().lower().strip() == 'zgemma':
-                self.isZGEMMA = True
-            else:
-                self.isZGEMMA = False
-        except Exception:
-            self.isZGEMMA = False
         self.checkTimer = eTimer()
         self.checkTimer.callback.append(self.refreshPic)
         self.checkTimer.start(10000, True)
@@ -39,16 +30,14 @@ class j00zekRefreshBingPicOfTheDay(Converter, object):
             self.Init = False
             self.checkTimer.start(86400000) #check once a day 86400000 = 1000ms * 60 * 60 * 24
         with open("/proc/sys/vm/drop_caches", "w") as f: f.write("1\n")
-        Console().ePopen('/usr/bin/python /usr/lib/enigma2/python/Components/j00zekBING.py')
+        Console().ePopen('/usr/bin/python /usr/lib/enigma2/python/Components/j00zekBING.py "mergePic=%s"' % self.AlternatePicPath)
         
     @cached
     def getText(self):
-        if config.plugins.j00zekCC.PiPbackground.value == 'n' and self.isVTI:
-            return '/usr/share/enigma2/BlackHarmony/bg_design/bg_pure_black_1920x1080.png'
-        elif not self.isVTI and os.path.exists(self.BingPic_png):
-            return self.BingPic_png
-        elif os.path.exists(self.BingPic_jpg) and not self.isZGEMMA:
-            return self.BingPic_jpg
+        if config.plugins.j00zekCC.PiPbackground.value == 'n':
+            return self.AlternatePicPath
+        elif os.path.exists(self.BingPic):
+            return self.BingPic
         else:
             return self.AlternatePicPath
         
--- e2components/Components/j00zekBING.py
+++ e2components/Components/j00zekBING.py
@@ -25,41 +25,74 @@ try:
 except Exception:
     from j00zekComponents import isINETworking, isPY2
 
-import json
 if isPY2() == True:
     from urllib import urlretrieve
 else:
     from urllib.request import urlretrieve
-import urllib
-import requests
-from os.path import exists as OsPathExists
-from os import system as OsSystem
 
-def getPicOfTheDay(CountryCode = 'pl_PL', downloadPathAndFileName = '/usr/share/enigma2/BlackHarmony/icons/BingPicOfTheDay.jpg'):
+import json, os, requests, sys, time, urllib
+
+def getPicOfTheDay(CountryCode = 'pl_PL', downloadPathAndFileName = '/usr/share/enigma2/BlackHarmony/icons/BingPicOfTheDay.jpg', mergePic = ''):
     retVal = False
     url = 'http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=%s' % CountryCode
 
     try:
         if isINETworking():
-            #response = requests.get(webURL, headers={'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:54.0) Gecko/20100101 Firefox/54.0', 'Accept-Charset': 'utf-8'}, timeout=1)
-            response = requests.get(url, timeout=1)
-            response = response.content.decode()
-            response = json.loads(response)
-            if 'images' in response:
-                images = response['images']
-                for i in range(len(images)):
-                    url = 'http://www.bing.com' + images[i]['url']
-                    urlretrieve(url, downloadPathAndFileName)
-                    if OsPathExists(downloadPathAndFileName):
-                        OsSystem('ffmpeg -y -i %s %s 2>/dev/null &' % (downloadPathAndFileName,downloadPathAndFileName.replace('.jpg','.png')))
-                        retVal = True
-                        break
+            if os.path.exists(downloadPathAndFileName) and (int(time.time()) - int(os.path.getmtime(downloadPathAndFileName))) < 86400:
+                print('to early to get new bing pic of the day')
+            else:
+                #response = requests.get(webURL, headers={'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:54.0) Gecko/20100101 Firefox/54.0', 'Accept-Charset': 'utf-8'}, timeout=1)
+                response = requests.get(url, timeout=1)
+                response = response.content.decode()
+                response = json.loads(response)
+                if 'images' in response:
+                    images = response['images']
+                    for i in range(len(images)):
+                        url = 'http://www.bing.com' + images[i]['url']
+                        urlretrieve(url, downloadPathAndFileName)
+                        if os.path.exists(downloadPathAndFileName):
+                            print('new bing pic of the day downloaded')
+                            retVal = True
+                            break
     except Exception as e:
         print(str(e))
-       
+    
+    #PIL
+    mergedPic = mergePic.replace('.png','-Bing.png').replace('.jpg','-Bing.jpg')
+    if mergePic == '':
+        print('mergePic not provided, nothing to merge bing with')
+    elif not os.path.exists(mergePic):
+        print('mergePic does not exist, nothing to merge bing with')
+    elif not os.path.exists(downloadPathAndFileName):
+        print('no bing picture, nothing to merge to %s' % mergePic)
+    elif retVal == False and os.path.exists(mergedPic) and (int(time.time()) - int(os.path.getmtime(mergedPic))) < 86400:
+        print('to early to merge new bing pic of the day')
+    else:
+        try:
+            from PIL import Image
+            background = Image.open(downloadPathAndFileName)
+            foreground = Image.open(mergePic)
+            background.paste(foreground, (0, 0), foreground.convert('RGBA'))
+            background = background.convert("P", palette=Image.ADAPTIVE, colors=256)
+            background.save(mergedPic, format="PNG", quality=6, optimize=True, progressive=True)
+            print('bing pic merged to %s and saved as %s' % (mergePic, mergedPic))
+        except Exception as e:
+            print(str(e))
+            os.system('opkg update; opkg install python3-pillow;opkg install python-pillow')
     return retVal
 
 
 #for tests outside e2
 if __name__ == '__main__':
-    getPicOfTheDay()
+    CountryCode = 'pl_PL'
+    downloadPathAndFileName = '/usr/share/enigma2/BlackHarmony/icons/BingPicOfTheDay.jpg'
+    mergePic = '/usr/share/enigma2/BlackHarmony/bg_design/EPGPig.png'
+    for myArg in sys.argv:
+        if '=' in myArg:
+            myArg = myArg.split('=', 1)
+            param = myArg[0].strip()
+            value = myArg[1].strip()
+            if param == 'CountryCode': CountryCode = value
+            elif param == 'downloadPathAndFileName': downloadPathAndFileName = value
+            elif param == 'mergePic': mergePic = value
+    getPicOfTheDay(CountryCode,downloadPathAndFileName,mergePic)
