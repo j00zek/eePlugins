c3a58b65ea6081f52409eddd8c37972047f69bb3
Zdzislaw <zdzislaw22@windowslive.com>
2023-12-28 07:11:21 +0100

    sync zadmario

--- zmodowanePrzezInnych/e2iplayer-zadmario/IPTVPlayer/hosts/hostwebstream.py
+++ zmodowanePrzezInnych/e2iplayer-zadmario/IPTVPlayer/hosts/hostwebstream.py
@@ -140,7 +140,7 @@ class HasBahCa(CBaseHostClass):
                         {'alias_id': 'wiziwig1.eu', 'name': 'wiziwig1.eu', 'title': 'http://wiziwig1.eu/', 'url': '', 'icon': 'http://i.imgur.com/yBX7fZA.jpg'},\
                         {'alias_id': 'djing.com', 'name': 'djing.com', 'title': 'https://djing.com/', 'url': 'https://djing.com/', 'icon': 'https://www.djing.com/newimages/content/c01.jpg'}, \
                         {'alias_id': 'nhl66.ir', 'name': 'nhl66.ir', 'title': 'https://nhl66.ir', 'url': 'https://api.nhl66.ir/api/sport/schedule', 'icon': 'https://nhl66.ir/cassets/logo.png'}, \
-                        {'alias_id': 'strims.top', 'name': 'strims.top', 'title': 'http://strims.top/', 'url': 'http://strims.top/', 'icon': ''}, \
+                        {'alias_id': 'strims.in', 'name': 'strims.in', 'title': 'http://strims.in/', 'url': 'http://strims.in/', 'icon': ''}, \
                        ]
 
     def __init__(self):
@@ -770,7 +770,7 @@ class HasBahCa(CBaseHostClass):
         for item in data:
             linkVideo = self.cm.ph.getSearchGroups(item, '''\shref=['"]([^"^']+?)['"]''')[0]
             if len(linkVideo) and not linkVideo.startswith('http'):
-                linkVideo = 'http://strims.top' + linkVideo
+                linkVideo = 'http://strims.in' + linkVideo
             if linkVideo.endswith('/') and 'class="f1' not in item:
                 params = {'name': "strumyk_cat"}
             else:
@@ -793,7 +793,7 @@ class HasBahCa(CBaseHostClass):
             linkVideo = self.cm.ph.getSearchGroups(item, '''\shref=['"]([^"^']+?)['"]''')[0]
             if len(linkVideo):
                 if not linkVideo.startswith('http'):
-                    linkVideo = 'http://strims.top' + linkVideo
+                    linkVideo = 'http://strims.in' + linkVideo
                 params['url'] = urlparser.decorateUrl(linkVideo, {'Referer': url})
 #                params['icon'] = self.cm.ph.getSearchGroups(item, '''\ssrc=['"]([^"^']+?)['"]''')[0]
                 self.addDir(params)
@@ -814,13 +814,13 @@ class HasBahCa(CBaseHostClass):
             linkVideo = self.cm.ph.getSearchGroups(tmp, '''src=['"]([^"^']+?)['"]''')[0]
             linkVideo = linkVideo.strip(' \n\t\r')
             if linkVideo.startswith('/live/'):
-                sts, tmp = self.cm.getPage('http://strims.top' + linkVideo)
+                sts, tmp = self.cm.getPage('http://strims.in' + linkVideo)
                 if not sts:
                     return []
                 linkVideo = self.cm.ph.getSearchGroups(tmp, '''src=['"]([^"^']+?)['"]''')[0]
                 linkVideo = linkVideo.strip(' \n\t\r')
             if len(linkVideo):
-                params = {'name': "strims.top"}
+                params = {'name': "strims.in"}
                 params['url'] = urlparser.decorateUrl(linkVideo, {'Referer': url})
                 params['title'] = self.up.getDomain(linkVideo)
                 self.addVideo(params)
@@ -832,7 +832,7 @@ class HasBahCa(CBaseHostClass):
             if _url.startswith('?'):
                 _url = url + _url
             if not _url.startswith('http'):
-                _url = 'http://strims.top' + _url
+                _url = 'http://strims.in' + _url
             sts, data = self.cm.getPage(_url)
             if sts:
                 tmp = CParsingHelper.getDataBeetwenNodes(data, ('<iframe', '>', 'allowfullscreen'), ('</iframe', '>'))[1]
@@ -846,7 +846,7 @@ class HasBahCa(CBaseHostClass):
                 if len(linkVideo) and linkVideo.startswith('//'):
                     linkVideo = 'http:' + linkVideo
                 if len(linkVideo) and not linkVideo.startswith('http'):
-                    linkVideo = 'http://strims.top' + linkVideo
+                    linkVideo = 'http://strims.in' + linkVideo
                     sts, data = self.cm.getPage(linkVideo)
                     tmp = CParsingHelper.getDataBeetwenNodes(data, ('<iframe', '>', 'src'), ('</iframe', '>'))[1]
                     if len(tmp):
@@ -863,7 +863,7 @@ class HasBahCa(CBaseHostClass):
                 linkVideo = linkVideo.replace('https://href.li/', '')
                 if '' == linkVideo:
                     continue
-                params = {'name': "strims.top"}
+                params = {'name': "strims.in"}
                 params['url'] = urlparser.decorateUrl(linkVideo, {'Referer': url})
                 params['title'] = self.cleanHtmlStr(item) + ' - ' + self.up.getDomain(linkVideo)
                 printDBG("StrumykTvDir params [%s]" % params)
@@ -938,7 +938,7 @@ class HasBahCa(CBaseHostClass):
             self.getWiziwig1List(self.currItem)
         elif name == 'nhl66.ir':
             self.getNhl66List(url)
-        elif name == 'strims.top':
+        elif name == 'strims.in':
             self.getStrumykTvList(url)
         elif name == 'strumyk_tv':
             self.getStrumykTvDir(url)
@@ -1010,7 +1010,7 @@ class IPTVHost(CHostBase):
             urlList = self.host.getMLBStreamTVLink(cItem)
         elif name == "wiziwig1.eu":
             urlList = self.host.getWiziwig1Link(cItem)
-        elif name == "strims.top":
+        elif name == "strims.in":
             urlList = self.host.getStrumykTvLink(url)
 
         if isinstance(urlList, list):
