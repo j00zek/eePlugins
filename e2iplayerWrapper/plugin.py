58fec6a42ae19dbcddda7b48a7969e0a24819f9b
Zdzislaw <zdzislaw22@windowslive.com>
2024-04-27 07:31:19 +0200

    fix działania dla starszych systemów.

--- e2iplayerWrapper/plugin.py
+++ e2iplayerWrapper/plugin.py
@@ -1,5 +1,5 @@
 #
-# e2iplayerWrapper do uruchamiania materialow dostpenych w e2iplayerze z listy kanalow
+# e2iplayerWrapper do uruchamiania materialow dostepnych w e2iplayerze z listy kanalow
 # dziala tylko na softach majacych zaimplementowana funkcje wrappera. Testowane na OpenATV 7.X
 # skladnia na liscie kanalow: <referencja>:http%3a//e2iplayer/<nazwa hosta>%3a<sciezka w hoscie>:nazwa kanalu na liscie
 # Przyklad:
@@ -16,6 +16,7 @@ def Plugins(**kwargs):
     return [PluginDescriptor(name="StreamlinkExtWrapper", description="StreamlinkExtWrapper", where=PluginDescriptor.WHERE_CHANNEL_ZAP, needsRestart = False, fnc=zap)]
 
 MYe2iPlayer = None
+MyHelper = None
 
 def e2iBatchCMD(batchCMD = ''):
     if batchCMD == '':
@@ -27,32 +28,45 @@ def e2iBatchCMD(batchCMD = ''):
             f.write(batchCMD)
             f.close()
 
-def zap(session, service, **kwargs):
     def END_e2iplayer(answer=None, lastPosition=None, clipLength=None, *args, **kwargs):
         e2iBatchCMD()
-    
+
+def zap(session, service, **kwargs):
     errormsg = None
     if service:
         try:
             serviceString = service.toString()
             url = serviceString.split(":")[10]
             if url != '' and url.startswith("http%3a//e2iplayer/"):
-                global MYe2iPlayer
-                batchCMD = url[len('http%3a//e2iplayer/'):] # expected result: "canalplus%3aTelewizja na żywo/Wszystkie kanały/CANAL+ DOMO HD"
+                print("j00zek:[ChannelSelection] zap > e2iplayer >>>")
+                global MYe2iPlayer, MyHelper
+                batchCMD = url[len('http%3a//e2iplayer/'):]
                 batchCMD = batchCMD.replace('%3a', ':')
                 e2iBatchCMD(batchCMD)
                 if MYe2iPlayer is None:
                     try:
                         from Components.PluginComponent import pluginComponent
                         pluginList = pluginComponent.getPlugins(PluginDescriptor.WHERE_PLUGINMENU)
-                        for weight, plugin in enumerate(pluginList, start=1):
-                            if plugin.name == 'E2iPlayer':
-                                MYe2iPlayer = plugin
-                                break
-                    except ImportError as e:
-                        print("[ChannelSelection] zap > MYe2iPlayer importing IPTVPlayer component error '%s'" % str(e))
+                    except Exception:
+                        from Components.PluginComponent import plugins
+                        pluginList = plugins.getPlugins(PluginDescriptor.WHERE_PLUGINMENU)
+                    for weight, plugin in enumerate(pluginList, start=1):
+                        if plugin.name == 'E2iPlayer':
+                            MYe2iPlayer = plugin
+                            break
+                if MyHelper is None:
+                    try:
+                        from Plugins.Extensions.e2iplayerWrapper.helperNP import Helper
+                        MyHelper = Helper()
+                    except Exception as e:
+                        MyHelper = False
+                        print("j00zek:[ChannelSelection] zap > exception loading Helper:", str(e))
                 if MYe2iPlayer:
+                    if MyHelper != False:
+                        MyHelper.init(batchCMD)
+                    print("j00zek:[ChannelSelection] zap > jumping to MYe2iPlayer")
                     MYe2iPlayer(session)
+                    print("j00zek:[ChannelSelection] zap > return from MYe2iPlayer")
         except Exception as e:
-            print("[ChannelSelection] zap > MYe2iPlayer failed %s" % str(e))
+            print("j00zek:[ChannelSelection] zap > MYe2iPlayer failed %s" % str(e))
     return (None, errormsg)
